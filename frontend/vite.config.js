2e19a55b376f9e0d74bdc8dc026198673301aac7
JinYunHwa <dbsghk320@gmail.com>
2025-08-29 13:45:22 +0900

    feat: git pages를 위한 설정 추가

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,37 +1,39 @@
 import { fileURLToPath, URL } from 'node:url'
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
-import dotenv from 'dotenv'
-import { resolve } from 'path'
 
-// .env 파일 로드
-dotenv.config()
 
-// 현재 작업 디렉토리를 환경 변수로 설정
-const PROJECT_ROOT = process.cwd()
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '') // .env, .env.development, .env.production 로드
 
-export default defineConfig({
-  plugins: [vue(), vueDevTools()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-    },
-  },
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://api.documento.click', // 백엔드 서버 주소 변경
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
+  // GitHub Pages(프로젝트 페이지) 기본 경로
+  const base = '/documento/'
+
+  return {
+    base,
+    plugins: [vue(), vueDevTools()],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
       },
     },
-  },
-  define: {
-    'process.env': {
-      ...process.env,
-      VITE_PROJECT_ROOT: PROJECT_ROOT,
-      VITE_APP_CLIENT_ID: process.env.VITE_APP_CLIENT_ID,
+    server: {
+      proxy: {
+        '/api': {
+          target: 'https://api.documento.click',
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ''),
+        },
+      },
     },
-  },
+
+    define: {
+    },
+
+    build: {
+      outDir: 'dist',
+      emptyOutDir: true,
+    },
+  }
 })
